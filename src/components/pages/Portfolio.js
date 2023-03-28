8ba19f01ac77b29ea1a2517bc0d7c9bf5e53dac1
j-faust <85812553+j-faust@users.noreply.github.com>
2023-03-27 20:42:54 -0400

    added final project link

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,29 +10,24 @@ export default function Portfolio(props) {
     return (
         <div className="container">
             <div className="row">
-                <div className=" col-md-2">
+                <div className=" col-md-3">
                     {/* using props to generate cards using the Card component */}
                     <Card imgsrc={require("./images/pexels-pixabay-209831.jpg")} title="Weather Dashboard App" 
                     projdesc="This Weather Dashboard App lets you check weather for your city and saves your previous searches for later use. 
                     It also give a 5 day forecast to plan your day!"
                     projlink="https://j-faust.github.io/weather-dashboard-challenge/" />
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-3">
                     <Card imgsrc={require("./images/pexels-sora-shimazaki-5668471.jpg")} title="Workday Planner App" 
                     projdesc="This Workday Planning App can help with planning and managing your day to day activities and meetings!" 
                     projlink="https://j-faust.github.io/workday-planner/"  />
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-3">
                     <Card imgsrc={require("./images/pexels-pixabay-39584 (1).jpg")} title="Random Password Generator" 
                     projdesc="The Random Password Generator will generate a random password for you to use!"
                     projlink="https://j-faust.github.io/javascript-module-challenge/" />
                 </div>
-                <div className="col-md-2">
-                    <Card imgsrc={require("./images/pexels-markus-spiske-1089438.jpg")} title="Coding Quiz App" 
-                    projdesc="This Coding Quiz App will help test your basic coding knowledge!" 
-                    projlink="https://j-faust.github.io/api-module-challenge/"   /> 
-                </div>
-                <div className="col-md-2">
+                <div className="col-md-3">
                     <Card imgsrc={require("./images/pexels-ivan-samkov-4164849 (1).jpg")} title="Workout Hub" 
                     projdesc="Social Media App To Post Workouts and Interact with other Workout Enthusiasts" 
                     projlink="https://workout-hub-merng.herokuapp.com/"   /> 
